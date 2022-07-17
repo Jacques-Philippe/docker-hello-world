2e977ab91f449f7213da585a3dd9c40acb98ed83
Jacques-Philippe Amiot <jacques.p.amiot@gmail.com>
2022-07-16 21:32:13 -0400

    Add todo app container start with credentials

---
+++ dev/start-dev-container-with-mysql-credentials.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+# Should be run from project (repo) root
+PORT=3000
+WORKING_DIRECTORY=app
+PROJECT_ROOT="$(pwd)"
+
+NETWORK_NAME="todo-app"
+MYSQL_CONTAINER_NETWORK_ALIAS="mysql"
+TABLE_NAME="todos"
+
+docker run -dp $PORT:3000 \
+    -w "/$WORKING_DIRECTORY" \
+    -v "$PROJECT_ROOT/$WORKING_DIRECTORY:/$WORKING_DIRECTORY" \
+    --network $NETWORK_NAME \
+    -e MYSQL_HOST=$MYSQL_CONTAINER_NETWORK_ALIAS \
+    -e MYSQL_USER=root \
+    -e MYSQL_PASSWORD=secret \
+    -e MYSQL_DB=$TABLE_NAME \
+    node:12-alpine \
+    sh -c "yarn install && yarn run dev"
+
+
